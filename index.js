6a39c2b6c2ec9f3000dff134fce438e4fc4a0aac
Sri Vignesh M S <vignesh@Sris-MacBook-Pro.local>
2020-10-05 19:07:21 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-
+// Initializing the port to connect to
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`The port has started ${PORT}`));
 
